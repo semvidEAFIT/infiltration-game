f36f2be0e49b83a85e3d2353b6210e51d8459d50
Luis M Ponce de leon <luisponcekun@gmail.com>
2013-07-02 14:06:58 -0500

    Fix #6 - Desarrollar script de objetivos

--- Unity_Project/Assets/scripts/src/ctrl/Level.cs
+++ Unity_Project/Assets/scripts/src/ctrl/Level.cs
@@ -2,14 +2,45 @@ using UnityEngine;
 using System.Collections;
 
 public class Level : MonoBehaviour {
+	
+	private static Level instance;
+	
+	public static Level Instance {
+		get {
+			return instance;
+		}
+	}
+	
+	private int objectiveCount;
+	
+	void Awake(){
+		if (instance == null) {
+            instance = this;
+        }
+        else {
+            Debug.LogError("Solo puede haber un level activo a la vez");
+            Destroy(this.gameObject);
+        }
+		
+		objectiveCount=0;
+	}
+	
+    
 
+	
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+	}
 	
+	
+	public void addObjective(){
+		objectiveCount++;
+	}
+	public void objectiveDone(){
+		objectiveCount--;
 	}
 }
--- Unity_Project/Assets/scripts/src/ctrl/Objective.cs
+++ Unity_Project/Assets/scripts/src/ctrl/Objective.cs
@@ -5,11 +5,19 @@ public class Objective : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		Level.Instance.addObjective();
+	}
 	
+	void Awake(){
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
+	}
 	
+	public void ObjectiveDone(){
+		Level.Instance.objectiveDone();
 	}
 }
