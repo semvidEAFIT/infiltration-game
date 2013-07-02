5f83b945f008900a15880c515a95cdacb00ae11c
MateoC <carvajalmunoz@gmail.com>
2013-07-02 14:35:03 -0500

    fix # 7

--- Unity_Project/Assets/scripts/src/ctrl/Objective.cs
+++ Unity_Project/Assets/scripts/src/ctrl/Objective.cs
@@ -2,14 +2,22 @@ using UnityEngine;
 using System.Collections;
 
 public class Objective : MonoBehaviour {
-
+	
 	// Use this for initialization
 	void Start () {
+		Level.Instance.addObjective();
+	}
 	
+	void Awake(){
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
+	}
 	
+	public void ObjectiveDone(){
+		Level.Instance.objectiveDone();
 	}
 }
