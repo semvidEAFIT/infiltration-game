c9182aae09523a8216ce3483549cb5dd0b5e6fd2
MateoC <carvajalmunoz@gmail.com>
2013-07-02 15:33:05 -0500

    Fixes #27 las personas ya se siguen

--- Unity_Project/Assets/scripts/src/characters/Person.cs
+++ Unity_Project/Assets/scripts/src/characters/Person.cs
@@ -8,7 +8,9 @@ public class Person : MonoBehaviour {
 	private List<Vector3> route;
 	private Vector3 destination;
 	public float speed;
-	
+	public GameObject follow;// se debe popner privado una ves se programe un rehen
+	private CharacterController cc;
+	public float distance;
 	private int healthPoints;
 	public int initialHealth=5;
 	
@@ -17,18 +19,24 @@ public class Person : MonoBehaviour {
 		route = new List<Vector3>();
 		destination = Vector3.zero;
 		healthPoints=initialHealth;
+		if(follow!=null){//se quita una ves se programe un rehen
+			destination = follow.transform.position;
+		}
+		cc = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(destination ==  Vector3.zero && route.Count > 0){
+		if(follow == null&&destination ==  Vector3.zero && route.Count > 0){
 			destination = route[0];
 			route.RemoveAt(0);
 		}else if(destination != Vector3.zero){
-			GetComponent<CharacterController>().SimpleMove ((destination-transform.position)*speed);
+			cc.SimpleMove ((destination-transform.position)*speed);
 			if((transform.position-destination).magnitude < 10){
 				destination = Vector3.zero;
 			}
+		}else if(follow != null && (follow.transform.position-transform.position).magnitude > distance){
+			destination =  follow.GetComponent<Person>().Destination;
 		}
 		
 		if(Input.GetMouseButton(0)){
@@ -48,6 +56,7 @@ public class Person : MonoBehaviour {
 			//person dead
 		}
 	}
+	
 	public void AddWayPoint(Vector3 point){
 		route.Add(point);
 	}
@@ -60,6 +69,17 @@ public class Person : MonoBehaviour {
 		}
 		set {
 			destination = value;
+		}
+	}
+	
+
+	public GameObject Follow {
+		get {
+			return this.follow;
+		}
+		set {
+			follow = value;
+			destination = follow.transform.position;
 		}
 	}
 	
