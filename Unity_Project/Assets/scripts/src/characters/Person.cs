004b2697f6f3792ec3549fbf939a8c4890f6e1bf
Luis M Ponce de leon <luisponcekun@gmail.com>
2013-07-02 14:28:17 -0500

    Fix #20 - Añadir salud en la clase persona

--- Unity_Project/Assets/scripts/src/characters/Person.cs
+++ Unity_Project/Assets/scripts/src/characters/Person.cs
@@ -2,14 +2,24 @@ using UnityEngine;
 using System.Collections;
 
 public class Person : MonoBehaviour {
-
+	
+	private int healthPoints;
+	public int initialHealth=5;
+	
 	// Use this for initialization
 	void Start () {
-	
+		healthPoints=initialHealth;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+	}
 	
+	public void takeDamage(int damage){
+		healthPoints -= damage;
+		
+		if (healthPoints<=0){
+			//person dead
+		}
 	}
 }
--- Unity_Project/Assets/scripts/src/ctrl/Objective.cs
+++ Unity_Project/Assets/scripts/src/ctrl/Objective.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 
 public class Objective : MonoBehaviour {
-
+	
 	// Use this for initialization
 	void Start () {
 		Level.Instance.addObjective();
