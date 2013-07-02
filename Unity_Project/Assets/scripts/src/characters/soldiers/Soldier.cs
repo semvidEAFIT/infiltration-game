6f084b855721cc159252b415d48e2eb956d06269
Juan Castillo <castillobgr@gmail.com>
2013-07-02 15:14:03 -0500

    Fixes #5

--- Unity_Project/Assets/scripts/src/characters/soldiers/Soldier.cs
+++ Unity_Project/Assets/scripts/src/characters/soldiers/Soldier.cs
@@ -4,7 +4,10 @@ using System.Collections;
 public class Soldier : Person {
 	
 	public Gun gun;
-	public Item item; 
+	public Item item;
+	public GameObject bullet;
+	public float forceMultiplier;
+	public float maxAccuracyAngleDelta;
 	
 	// Use this for initialization
 	void Start () {
@@ -13,6 +16,15 @@ public class Soldier : Person {
 	
 	// Update is called once per frame
 	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			shoot();
+		}
+	}
 	
+	private void shoot(){
+		float angleDelta = Random.Range(-maxAccuracyAngleDelta, maxAccuracyAngleDelta) * Mathf.Deg2Rad;
+		float deltaX = Mathf.Sin(angleDelta);
+		GameObject shotBullet = (GameObject)GameObject.Instantiate(bullet, transform.forward, transform.rotation);
+		shotBullet.rigidbody.AddForce((transform.forward.x + deltaX) * forceMultiplier, 0, (transform.forward.z) * forceMultiplier);
 	}
 }
