b0e2ea5aa164ed4b79af574f438960fa9b47179a
Juan Castillo <castillobgr@gmail.com>
2013-07-03 15:14:21 -0500

    Fixes #54

--- Unity_Project/Assets/scripts/src/props/weapons/throwable explosives/Grenade.cs
+++ Unity_Project/Assets/scripts/src/props/weapons/throwable explosives/Grenade.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Grenade : ThrowableExplosive {
 	
-	public Grenade(float areaOfEffectRadius) : base(reaOfEffectRadius){
+	public Grenade(float areaOfEffectRadius, float grenadeDamage, float secondsUntilExplosion) : base(reaOfEffectRadius, grenadeDamage, secondsUntilExplosion){
 	}
 	
 }
