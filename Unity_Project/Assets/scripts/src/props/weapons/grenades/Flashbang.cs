c641b47a17366f29497398888c94ed86c903fb88
Juan Castillo <castillobgr@gmail.com>
2013-07-04 11:16:01 -0500

    fixes #71 . Modificaciones en la arquitectura, creación de clase abstracta Item.

---
+++ Unity_Project/Assets/scripts/src/props/weapons/grenades/Flashbang.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class Flashbang : Grenade {
+
+	public override void Start () {
+	
+	}
+	
+	public override void Update () {
+	
+	}
+	
+	public override void Activate(){
+		
+	}
+	
+	public override void Explode(){
+		
+	}
+}
