68c7ea05a96c2c68bfc182d72cb162afa599b16a
Juan Castillo <castillobgr@gmail.com>
2013-07-04 16:17:35 -0500

    fix #93 flashbang early prototype (can't tell between people and environment)

--- Unity_Project/Assets/scripts/src/characters/soldiers/Soldier.cs
+++ Unity_Project/Assets/scripts/src/characters/soldiers/Soldier.cs
@@ -28,8 +28,12 @@ public class Soldier : Person {
 			Shoot();
 		}
 		if(Input.GetKeyDown(KeyCode.G)){
-			GameObject grenade = GameObject.Instantiate(Resources.Load("Grenade"), this.gameObject.transform.position + this.gameObject.transform.forward, this.gameObject.transform.rotation) as GameObject;
+			GameObject grenade = GameObject.Instantiate(Resources.Load("fragGrenade"), this.gameObject.transform.position + this.gameObject.transform.forward, this.gameObject.transform.rotation) as GameObject;
 			grenade.GetComponent<FragGrenade>().Activate();
+		}
+		if(Input.GetKeyDown(KeyCode.F)){
+			GameObject grenade = GameObject.Instantiate(Resources.Load("flashGrenade"), this.gameObject.transform.position + this.gameObject.transform.forward, this.gameObject.transform.rotation) as GameObject;
+			grenade.GetComponent<Flashbang>().Activate();
 		}
 		if(Input.GetKey(KeyCode.A)){
 			transform.Translate(new Vector3(1, 0, 0));
--- Unity_Project/Assets/scripts/src/props/weapons/grenades/Flashbang.cs
+++ Unity_Project/Assets/scripts/src/props/weapons/grenades/Flashbang.cs
@@ -4,6 +4,16 @@ using System.Collections;
 public class Flashbang : Grenade {
 
 	public override void Explode(){
-
+		Collider [] hitColliders = Physics.OverlapSphere(transform.position, AOERadius);
+        
+        foreach (Collider c in hitColliders) {
+			try{
+            	c.gameObject.renderer.material.color = Color.black;
+			}
+			catch{
+				//OTHERWISE
+			}
+        }
+		Destroy(this.gameObject);
 	}
 }
