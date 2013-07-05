076773aab2c9077dc32bcec7f9666c7aa3014de4
Luis M Ponce de leon <luisponcekun@gmail.com>
2013-07-05 13:22:39 -0500

    Fix #106 - Arreglar emision de particulas cuando se dispare

--- Unity_Project/Assets/scripts/src/props/weapons/guns/SubmachineGun.cs
+++ Unity_Project/Assets/scripts/src/props/weapons/guns/SubmachineGun.cs
@@ -4,8 +4,7 @@ using System.Collections;
 public class SubmachineGun : Gun {
 
 	private int bulletsInBurst = 3;
-	private float bulletSpeed = 1;
-	
+	private float bulletSpeed = 50;
 	private GameObject bullet;
 
 	public SubmachineGun(GameObject ownerGameObject, int maxGunAmmo, int gunMagsLeft, float bulletDamage, float accuracyDelta, float shootingForce) : base(ownerGameObject, maxGunAmmo, gunMagsLeft, bulletDamage, accuracyDelta, shootingForce) {
@@ -14,7 +13,8 @@ public class SubmachineGun : Gun {
 
 	public override void Fire(){
 		for (int i=0; i < bulletsInBurst && bulletsLeft > 0; i++){
-			InstantiateBullet();
+			InstantiateBullet(bulletSpeed);
+			Debug.Log(i);
 		}
 
 		if(bulletsLeft <= 0){
@@ -28,27 +28,5 @@ public class SubmachineGun : Gun {
 		}
 	}
 
-	private void InstantiateBullet(){
-		bulletsLeft--;
-		float angleDelta = Random.Range(-this.accuracyDelta, this.accuracyDelta) * Mathf.Deg2Rad;
-		float deltaX = Mathf.Sin(angleDelta);
-		Vector3 dir = new Vector3(this.owner.transform.forward.x + deltaX, 0, this.owner.transform.forward.z);
-		Ray ray = new Ray(this.owner.transform.position + this.owner.transform.forward, dir);
-  		RaycastHit hit;
-		//TODO: IMPLEMENTAR DISTANCIA DE BALAS (TERCER PARAMETRO EN RAYCAST)
-    	if(Physics.Raycast(ray, out hit)){
-			//TODO: SE DEBE DEFINIR TODA LA DINÁMICA DE DAMAGE INFLICTION (SEGUIRA CON COLLIDERS??).
-			try{
-				hit.collider.gameObject.GetComponent<Person>().TakeDamage(damage);
-			}
-			catch{
-				//TODO:	OTHERWISE...
-			}
-		}
-		owner.gameObject.GetComponent<NoiseMaker>().MakeNoise();
-		
-		ParticleSystem particleSystem = owner.gameObject.GetComponent<ParticleSystem>();
-		particleSystem.Emit(1);
-
-	}
+	
 }
