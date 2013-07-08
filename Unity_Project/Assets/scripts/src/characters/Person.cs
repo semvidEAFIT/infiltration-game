3c972781d8b3aeb4715f678fb377bb61bd4293ba
Luis M Ponce de leon <luisponcekun@gmail.com>
2013-07-08 11:20:49 -0500

    Fix #149 - Integrar senses

--- Unity_Project/Assets/scripts/src/characters/Person.cs
+++ Unity_Project/Assets/scripts/src/characters/Person.cs
@@ -117,7 +117,9 @@ public class Person : MonoBehaviour {
 	}
 	
 	public virtual void View(RaycastHit[] gs){
-		
+		foreach(RaycastHit hit in gs){
+			Debug.DrawRay(transform.position, (hit.transform.position-transform.position).normalized * 20, Color.red);
+		}		
 	}
 	#endregion
 }
